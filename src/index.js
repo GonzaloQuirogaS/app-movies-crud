5fbb079e04123235d212c46e13b5a1eafa93ef7b
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-11-18 21:51:00 -0300

    2. Primer endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const v1Router = require("./v1/route");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
